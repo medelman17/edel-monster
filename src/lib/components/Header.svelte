d9704820b282d6534f205570160c634b38c031f3
medelman17 <mike.edelman@gmail.com>
2021-11-16 00:11:19 -0500

    first commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import NavBar from '$lib/components/NavBar.svelte';
+	import NavItem from '$lib/components/NavItem.svelte';
+</script>
+
+<header>
+	<div class="corner">
+		<a href="/"><span class="h3">Michael Edelman</span></a>
+	</div>
+	<nav>
+		<NavBar>
+			<NavItem label="Home" path="/" />
+			<NavItem label="Blog" path="/blog" />
+			<NavItem label="About" path="/about" />
+			<NavItem label="Contact" path="/contact" />
+		</NavBar>
+	</nav>
+</header>
+
+<style lang="postcss">
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+	nav {
+		display: flex;
+		justify-content: flex-end;
+		flex: 1;
+	}
+	.corner {
+		height: 3rem;
+		flex: 1;
+	}
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner a span {
+		color: var(--color-red);
+		font-weight: 700;
+		margin-top: 0;
+		padding-bottom: 0;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,44 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>Tbh edison bulb hot chicken.</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+	<p>
+		Coloring book pabst tacos tote bag vaporware gochujang pop-up adaptogen green juice
+		cold-pressed. Tattooed woke adaptogen seitan synth pinterest helvetica portland tote bag unicorn
+		mlkshk. Fam everyday carry try-hard, shaman schlitz banh mi umami sartorial mustache. Pop-up
+		bitters hot chicken cold-pressed. Pitchfork XOXO twee, tattooed marfa franzen coloring book.
+	</p>
+	<p>
+		I'm baby austin affogato helvetica lomo glossier next level truffaut DIY swag coloring book
+		migas brunch +1 twee. <a href="/">Before they sold out</a> try-hard man braid vape. Gentrify chartreuse
+		poke kogi lo-fi, typewriter offal vexillologist meh paleo ennui. Actually cronut pork belly, mumblecore
+		flannel meggings gluten-free williamsburg flexitarian. Readymade snackwave street art, shabby chic
+		aesthetic pok pok venmo migas brunch mlkshk.
+	</p>
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
